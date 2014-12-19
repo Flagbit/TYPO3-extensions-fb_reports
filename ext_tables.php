5aa8562998f1169760b3143c090ab54ef4b60e4d
Frederic Gaus <gaus@flagbit.de>
2014-12-19 16:27:09 +0100

    [BUGFIX] Implements a more robust version comparison
    This bugfix implements a more robust version comparison in
    ext_tables.php. This fixes a bug in TYPO3 6.1.X

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,9 @@
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
 if (TYPO3_MODE === 'BE') {
-	if (class_exists('t3lib_div') && (t3lib_div::int_from_ver(TYPO3_version) < t3lib_div::int_from_ver('6.0.0'))) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['typo3'][] = 'tx_fbreports_report_status_typo3statusv4';
-	} else {
+	if (class_exists('\\TYPO3\\CMS\\Core\\Utility\\VersionNumberUtility') && (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('6.0.0'))) {
 		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['typo3'][] = 'Flagbit\\FbReports\\Report\\Status\\Typo3Status';
+	} else {
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['typo3'][] = 'tx_fbreports_report_status_typo3statusv4';
 	}
 }
-
-?>

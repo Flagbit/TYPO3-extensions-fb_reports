c32abcefea53cc1d13b250f688b85035aaf769dc
Frederic Gaus <gaus@flagbit.de>
2014-12-31 13:45:02 +0100

    [FEATURE] Add Check for directory listings and sensitiveFileDownloads
    Added a check to look for misconfigurated directory settings and
    one for checking sensitive downloadable files. (e.g. sql-files)

---
+++ Classes/Report/Status/SecurityStatus.php
@@ -0,0 +1,139 @@
+<?php
+namespace Flagbit\FbReports\Report\Status;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Performs basic checks about the TYPO3 install
+ *
+ * @author Frederic Gaus <frederic.gaus@flagbit.de>
+ */
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Lang\LanguageService;
+use TYPO3\CMS\Reports\Status;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+
+class SecurityStatus implements \TYPO3\CMS\Reports\StatusProviderInterface {
+
+	protected $filesExtensionsToFind = array ('*.sql','*.sql.gz','*.sql.gzip','*.sql.bzip2','*.sql.bz2','*.tar','*.tar.gz','*.tar.bz2');
+
+	/**
+	 * Return status checks
+	 *
+	 * @return array List of statuses
+	 */
+	public function getStatus() {
+		$statuses = array();
+		$statuses['listFilesInFolders'] = $this->listFilesInFoldersStatus();
+		$statuses['sensitiveFilesDownloadable'] = $this->sensitiveFilesDownloadableStatus();
+		return $statuses;
+	}
+
+	/**
+	 * Checks for the ability to list files in some folders like typo3conf/ typo3temp/
+	 *
+	 * @return Status An object representing whether typo3conf/, typo3temp/ can be listed
+	 */
+	protected function listFilesInFoldersStatus() {
+		$value = $this->getLanguageService()->getLL('status_disabled');
+		$severity = Status::OK;
+		$message = '';
+		$foldersToTest = array(
+			'typo3conf',
+			'typo3temp',
+		);
+
+		$insecureFolders = array();
+		$typo3RequestHost = GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST');
+		foreach ($foldersToTest as $folderToTest) {
+			$urlToTest = $typo3RequestHost . '/' . $folderToTest . '/';
+			$headers = GeneralUtility::getUrl($urlToTest, 2);
+			if (preg_match('/^.*200.*\\n/', $headers)) {
+				if (strlen(GeneralUtility::getUrl($urlToTest)) > 0) {
+					$insecureFolders[$folderToTest] = $urlToTest;
+				}
+			}
+		}
+
+		if (!empty($insecureFolders)) {
+			$value = $this->getLanguageService()->getLL('status_insecure');
+			$severity = Status::ERROR;
+			$insecureFoldersLinks = array();
+			foreach ($insecureFolders as $insecureFolder) {
+				$insecureFoldersLinks[] = '<a href="' . htmlspecialchars($insecureFolder) . '" >' . $insecureFolder . '</a>';
+			}
+			$message = sprintf(LocalizationUtility::translate('status_listFilesInFoldersInfo', 'fb_reports'), implode(', ', $insecureFoldersLinks));
+		}
+		return GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status', LocalizationUtility::translate('status_listFilesInFolders', 'fb_reports'), $value, $message, $severity);
+	}
+
+	/**
+	 * Checks if in root folder and in the the first level of folders there are sensitive files that can be downloaded
+	 *
+	 * @return Status An object representing whether there are sensitive files in root folder or in the the first level of folders that can be downloaded
+	 */
+	protected function sensitiveFilesDownloadableStatus() {
+		$value = $this->getLanguageService()->getLL('status_none');
+		$severity = Status::OK;
+		$message = '';
+
+		$foldersToSearchForFiles = new \DirectoryIterator(PATH_site);
+
+		$filesToTestDownload = array();
+		foreach ($foldersToSearchForFiles as $folderToSearchForFile) {
+			if($folderToSearchForFile->isDir() && $folderToSearchForFile->getFilename() !== '..') {
+				foreach ($this->filesExtensionsToFind as $fileExtensionToFind) {
+					$iterator = new \GlobIterator(PATH_site . str_replace('./', '', $folderToSearchForFile->getFilename() . '/') . trim($fileExtensionToFind));
+					/** @var \SplFileInfo $splFileInfo */
+					foreach ($iterator as $splFileInfo) {
+						$filesToTestDownload[] = substr($splFileInfo->getPathname(), strlen(PATH_site));
+						break(1);
+					}
+				}
+			}
+		}
+
+		$typo3RequestHost = GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST');
+		$insecureFilesToDownload = array();
+		foreach ($filesToTestDownload as $fileToTestDownload) {
+			$downloadTestFileUrl = $typo3RequestHost . '/' . $fileToTestDownload;
+			$headers = GeneralUtility::getUrl($downloadTestFileUrl, 2);
+			if (preg_match('/^.*200.*\\n/', $headers)) {
+				$insecureFilesToDownload[] = $downloadTestFileUrl;
+			}
+		}
+
+		if (!empty($insecureFilesToDownload)) {
+			$value = $this->getLanguageService()->getLL('status_insecure');
+			$severity = Status::ERROR;
+			$insecureFileToDownloadLinks = array();
+			foreach ($insecureFilesToDownload as $insecureFileToDownload) {
+				$insecureFileToDownloadLinks[] = '<a href="' . htmlspecialchars($insecureFileToDownload) . '" >' . $insecureFileToDownload . '</a>';
+			}
+			$message = sprintf(LocalizationUtility::translate('status_sensitiveFilesDownloadableInfo', 'fb_reports'), $this->filesExtensionsToFind, implode('<br />', $insecureFileToDownloadLinks));
+		}
+
+		return GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status', LocalizationUtility::translate('status_sensitiveFilesDownloadable', 'fb_reports'), $value, $message, $severity);
+	}
+
+	/**
+	 * Returns Language Service
+	 *
+	 * @return LanguageService
+	 */
+	private function getLanguageService() {
+		return $GLOBALS['LANG'];
+	}
+
+}
--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,9 @@
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
 if (TYPO3_MODE === 'BE') {
-	if (class_exists('\\TYPO3\\CMS\\Core\\Utility\\VersionNumberUtility') && (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('6.0.0'))) {
+	if (class_exists('\\TYPO3\\CMS\\Core\\Utility\\VersionNumberUtility') && (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) > \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('6.0.0'))) {
 		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['typo3'][] = 'Flagbit\\FbReports\\Report\\Status\\Typo3Status';
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['security'][] = 'Flagbit\\FbReports\\Report\\Status\\SecurityStatus';
 	} else {
 		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['typo3'][] = 'tx_fbreports_report_status_typo3statusv4';
 	}

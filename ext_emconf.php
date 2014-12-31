fe2c931d7e351d47c8b4e8ef0653a118d3a3c714
Frederic Gaus <gaus@flagbit.de>
2014-12-31 13:47:33 +0100

    [TASK] Update version compability

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.0.0-5.5.99',
-			'typo3' => '4.5.0-6.2.99',
+			'typo3' => '4.5.0-7.0.99',
 		),
 		'conflicts' => 
 		array (
